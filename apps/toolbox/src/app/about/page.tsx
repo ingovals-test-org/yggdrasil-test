ad31f4fb55f2133ba4296aba6fc2dac307b909c2
Ingólfur Valsson <ingolfur@programm.is>
2026-03-04 14:31:18 +0000

    feat(toolbox): Add about page
    new about page added

---
+++ apps/toolbox/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Label } from '@yggdrasil/ui/label'
+
+export default async function Index() {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center gap-4'>
+      <h1 className='font-bold text-4xl'>About page</h1>
+      <p className='text-gray-600 text-lg'>Your one-stop shop for all your needs</p>
+      <Label>Get Started</Label>
+    </div>
+  )
+}
